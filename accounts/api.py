a7392bdf1b6caec5716c34334a61934f7cd9f1c5
hamiddodge <asadiyan.hamid@gmail.com>
2021-08-17 14:26:28 +0430

    account services

--- accounts/api.py
+++ accounts/api.py
@@ -21,6 +21,7 @@ class AccountViewSets(mixins.CreateModelMixin,
     def create(self, request, *args, **kwargs):
         serializer = AccountCreateSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
+        # serializer.save(customer=request.user) here we pass the instance it self for serializer
+        serializer.save(customer_id=request.user.id)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
--- users/models.py
+++ users/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
 
-class Customer(User):
+class Customer(AbstractUser):
     identity_code = models.CharField(max_length=10)
