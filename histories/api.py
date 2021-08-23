aa54b7e8fc8b916e22700fe75e1bde8d1141e58a
hamiddodge <asadiyan.hamid@gmail.com>
2021-08-23 10:17:37 +0430

    cleaning

--- histories/api.py
+++ histories/api.py
@@ -16,16 +16,3 @@ class HistoryViewSet(  # mixins.CreateModelMixin,
     model = History
     queryset = model.objects.all()
     serializer_class = HistorySerializer
-
-    # @action(detail=False, methods=['GET'])
-    # def get_history(self, request, *args, **kwargs):
-    #     model = History
-    #     queryset = model.objects.filter(transfer_source_id=request.user.id)
-    #
-    #     page = self.paginate_queryset(queryset)
-    #     if page is not None:
-    #         serializer = HistorySerializer(page, many=True)
-    #         return self.get_paginated_response(serializer.data)
-    #
-    #     serializer = HistorySerializer(queryset, many=True)
-    #     return Response(serializer.data)
--- histories/urls.py
+++ histories/urls.py
@@ -1,1 +1,8 @@
+from rest_framework.routers import DefaultRouter
 
+from .api import HistoryViewSet
+
+router = DefaultRouter()
+router.register('histories', HistoryViewSet, 'histories')
+
+urlpatterns = router.urls
